ef5e9f6d7b77fda8a98d735540938b40bc793183
lamdayne123 <acccuoi400@gmail.com>
2026-07-18 22:20:13 +0700

    Change background image format from jpg to jpeg

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
     className="min-h-screen bg-cover bg-center bg-fixed text-white"
     style={{
       backgroundImage:
-        "linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.55)), url('/images/background.jpg')",
+        "linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.55)), url('/images/background.jpeg')",
     }}
   >
       {/* Navbar */}
